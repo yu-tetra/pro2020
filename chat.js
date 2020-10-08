04f47b46fb5698d81111bfc7df33fc65bb52d993
yu-tetra <y.sasaki.sys19@morijyobi.ac.jp>
2020-10-08 11:53:08 +0900

    res

--- chat.js
+++ chat.js
@@ -5,21 +5,24 @@ var place = db.collection("rooms").doc("roomID").collection("chatroom");
 const btn = document.getElementById("btn");
 var message = document.getElementById("message");
 const disp = document.getElementById("tdisp");
-var time = firebase.firestore.FieldValue.serverTimestamp()
 
 //送信処理
 btn.addEventListener('click', ()=> {
    console.log(message.value);
    place.add({
-      time: message.value,
-      timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      chat: message.value,
+      time: firebase.firestore.FieldValue.serverTimestamp()
    });
 });
 
 //受信処理
-place.get().then(function(querySnapshot) {
-   querySnapshot.forEach(function(doc) {
-       // doc.data() is never undefined for query doc snapshots
-       console.log(doc.id, " => ", doc.data());
-   });
+place.orderBy("time").onSnapshot((snapshot) => {
+   snapshot.docChanges().forEach((change) => {
+      // doc.data() is never undefined for query doc snapshots
+      console.log(change.doc.id, " => ", change.doc.data);
+      
 });
+});
+
+      var chat = change.doc.data();
+      $("#tdisp").append('<td>'+chat.chat+'</td>');
