9bee3323009daad084eafc8721915406f7780993
Takahashi-11 <r.takahashi.sys19@morijyobi.ac.jp>
2020-10-08 11:45:03 +0900

    'text'

--- canvas.js
+++ canvas.js
@@ -17,6 +17,7 @@ $('#canvas').attr('height', h);
  var cnvBold = 3;  // 線の太さ
  var clickFlg = 0;  // クリック中の判定 1:クリック開始 2:クリック中
  var bgColor = "rgb(0,70,67)";
+ var color = '#ffffff';   //カラーコード保持変数
  var rgba_code = "255, 255, 255, 1"; //線の色
 
  // canvas上でのイベント
@@ -65,6 +66,7 @@ $('#canvas').attr('height', h);
  });
 
 
+ //カラーピッカーここから
 /// 長押しを検知する閾値
 var LONGPRESS = 500;
 /// 長押し実行タイマーのID
@@ -93,7 +95,7 @@ function showpallete(){
 //jQueryから色コード取得
 var picker = document.getElementById('colorPicker');
 function colorChanged(event){
-  var color = event.detail[0];
+  color = event.detail[0];
   console.log('color:' + color);
   picker.value = color;
   cnvColor = getRgba(color);
@@ -113,11 +115,14 @@ function getRgba(color,alpha = 1){
 }
 
 //カラーピッカーの閉じボタン
-document.getElementById("close").onclick = function(){
-  var wObjballoon	= document.getElementById("colorPicker");
-  wObjballoon.className = "palette1";
+document.getElementById("c_close").onclick = function(){
+  var wObjballoon1	= document.getElementById("colorPicker");
+  wObjballoon1.className = "palette1";
 };
+//カラーピッカーここまで
 
+
+//メニューここから
 //メニュー（クリック）
 document.getElementById("menu").onclick = function(){
   showmenu();
@@ -133,6 +138,7 @@ function showmenu(){
   }
 }
 
+//退出確認
 document.getElementById("exit").onclick = function(){
   var exitresult = confirm("部屋を退出しますか？");
 
@@ -142,3 +148,84 @@ document.getElementById("exit").onclick = function(){
     return;
   }
 }
+
+//メニューの閉じボタン
+  document.getElementById("m_close").onclick = function(){
+    var wObjballoon2	= document.getElementById("menulist");
+    wObjballoon2.className = "menulist1";
+  };
+  //メニューここまで
+
+
+//テキストツールここから
+//文字挿入用の座標を保持する変数
+var mouseX;
+var mouseY;
+
+//フォーム呼び出し
+  document.getElementById("txt").onclick = function(){
+    //（あとでマウスカーソル形状変更を追加）
+    //処理をペンでの描画からテキストフォーム表示に切り替える・フォームの表示
+    textform_positon();
+  };
+
+  //テキスト入力フォーム表示・非表示
+  function showform(){
+    var formstatus = document.getElementById("txtform");
+    if (formstatus.className == "txtform1"){
+      formstatus.className = "txtform";
+    }else{
+      formstatus.className = "txtform1";
+    }
+  }
+
+  //クリック位置にテキスト入力フォームを表示
+  function textform_positon(){
+    $(cnvs).on('click.text',function(e) {
+      //クリックした際描画されないようペンを透明に
+      cnvColor = "255, 255, 255, 0";
+
+      //座標取得（canvasの左上を基準。-2ずつしているのはborderの分）
+      var rect = e.target.getBoundingClientRect();
+      mouseX = e.clientX - Math.floor(rect.left) - 2;
+      mouseY = e.clientY - Math.floor(rect.top) - 2;
+
+      //css書き換え（表示位置）
+      var txtform = document.getElementById('txtform');
+
+      txtform.style.left = mouseX;
+      txtform.style.top = mouseY;
+
+      //表示
+      showform();
+    });
+  }
+
+  //テキスト入力フォームの閉じボタン
+  document.getElementById("t_close").onclick = function(){
+    var wObjballoon3	= document.getElementById("txtform");
+    wObjballoon3.className = "txtform1";
+    //クリックした際にフォームを出さないようにする
+    $(cnvs).off('click.text');
+    //描画に戻す
+    cnvColor = rgba_code; //線の色
+    cnvBold = 3;  // 線の太さ
+  };
+
+  //入力された文字列の受け取り
+  document.getElementById("sendtxt").onclick = function gettxt(){
+    var text = document.getElementById("inputtxt").value;
+    drawtext(text);
+  }
+
+  //キャンバスに文字を書く
+  function drawtext(text){
+    //文字のスタイル
+    ctx.font = '32px serif';
+    ctx.fillStyle = color;
+
+    //文字描画位置の指定・文字描画
+    ctx.textAlign = 'left'; 
+    ctx.fillText(text,mouseX,mouseY);
+}
+//テキストツールここまで
