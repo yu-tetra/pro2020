f83e6a2dcf3b5bc576a6c348781c95e0ce4fc1ae
yu-tetra <y.sasaki.sys19@morijyobi.ac.jp>
2020-10-09 12:31:45 +0900

    res

--- chat.js
+++ chat.js
@@ -4,25 +4,18 @@ var place = db.collection("rooms").doc("roomID").collection("chatroom");
 
 const btn = document.getElementById("btn");
 var message = document.getElementById("message");
-const disp = document.getElementById("tdisp");
-
 //送信処理
 btn.addEventListener('click', ()=> {
-   console.log(message.value);
    place.add({
       chat: message.value,
       time: firebase.firestore.FieldValue.serverTimestamp()
    });
 });
+document.addEventListener("submit", (e)=>{
+   e.preventDefault();
+ });
 
 //受信処理
-place.orderBy("time").onSnapshot((snapshot) => {
-   snapshot.docChanges().forEach((change) => {
-      // doc.data() is never undefined for query doc snapshots
-      console.log(change.doc.id, " => ", change.doc.data);
-      
-});
-});
 
-      var chat = change.doc.data();
-      $("#tdisp").append('<td>'+chat.chat+'</td>');
+
+      
---
+++ test.js
@@ -0,0 +1,15 @@
+var db = firebase.firestore();
+
+var place = db.collection("rooms").doc("roomID").collection("chatroom").orderBy("time");
+
+
+location.reload();
+place.onSnapshot((snapshot)=> {
+    snapshot.docChanges().forEach((change) => {
+       if(change.type === "added"){
+          // doc.data() is never undefined for query doc snapshots
+          $('#tdisp').append('<tr><td>'+change.doc.data().chat+'</td></tr>');
+          console.log(change.doc.data())
+       }
+    });
+ });
