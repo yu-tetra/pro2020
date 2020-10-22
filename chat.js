f669c23bed13bad9672784147285e536ad299c08
yu-tetra <y.sasaki.sys19@morijyobi.ac.jp>
2020-10-22 10:18:45 +0900

    test

--- chat.js
+++ chat.js
@@ -1,8 +1,7 @@
 var db = firebase.firestore();
-
 var place = db.collection("rooms").doc("roomID").collection("chatroom");
 
-const btn = document.getElementById("btn");
+var btn = document.getElementById("btn");
 var message = document.getElementById("message");
 //送信処理
 btn.addEventListener('click', ()=> {
